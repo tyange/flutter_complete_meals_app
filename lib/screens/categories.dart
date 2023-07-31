c357a2964f7496e7c0e8a4a1a818d3ea7cf845d3
tyange <usun16@gmail.com>
2023-07-31 11:18:38 +0900

    add category item

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_meals_app/data/dummy_data.dart';
+import 'package:flutter_complete_meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,19 +12,17 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('Pick your category'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text('1', style: TextStyle(color: Colors.white)),
-          Text('2', style: TextStyle(color: Colors.white)),
-          Text('3', style: TextStyle(color: Colors.white)),
-          Text('4', style: TextStyle(color: Colors.white)),
-          Text('5', style: TextStyle(color: Colors.white)),
-          Text('6', style: TextStyle(color: Colors.white)),
+        children: [
+          // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );

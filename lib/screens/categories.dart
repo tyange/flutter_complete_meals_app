a571c38cc1a9c5da4dec81b55eaa5e5f545c1566
tyange <usun16@gmail.com>
2023-08-01 23:27:52 +0900

    wire up categories item with meals screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_meals_app/data/dummy_data.dart';
+import 'package:flutter_complete_meals_app/models/category.dart';
+import 'package:flutter_complete_meals_app/screens/meals.dart';
 import 'package:flutter_complete_meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +39,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectedCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );

294f0594cd8b6465ff916e147d73e1e000597d6d
tyange <usun16@gmail.com>
2023-08-05 19:45:12 +0900

    added main drawer

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -3,6 +3,7 @@ import 'package:flutter_complete_meals_app/models/meal.dart';
 
 import 'package:flutter_complete_meals_app/screens/categories.dart';
 import 'package:flutter_complete_meals_app/screens/meals.dart';
+import 'package:flutter_complete_meals_app/widgets/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -69,6 +70,7 @@ class _TabsScreenState extends State<TabsScreen> {
           activePageTitle,
         ),
       ),
+      drawer: const MainDrawer(),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,

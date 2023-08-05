65292219da5e091214d469480d738370266b391f
tyange <usun16@gmail.com>
2023-08-05 20:35:17 +0900

    added filters screen

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -49,6 +49,13 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
+  void _setScreen(String identifier) {
+    if (identifier == 'filters') {
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget activePage = CategoriesScreen(
@@ -70,7 +77,9 @@ class _TabsScreenState extends State<TabsScreen> {
           activePageTitle,
         ),
       ),
-      drawer: const MainDrawer(),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  const MainDrawer({super.key});
+  const MainDrawer({
+    super.key,
+    required this.onSelectScreen,
+  });
+
+  final void Function(String identifier) onSelectScreen;
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: Column(
-      children: [
-        DrawerHeader(
+      child: Column(
+        children: [
+          DrawerHeader(
             padding: const EdgeInsets.all(20),
             decoration: BoxDecoration(
                 gradient: LinearGradient(colors: [
@@ -32,8 +37,44 @@ class MainDrawer extends StatelessWidget {
                       ),
                 )
               ],
-            ))
-      ],
-    ));
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.restaurant,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Meals',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24,
+                  ),
+            ),
+            onTap: () {
+              onSelectScreen('meals');
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.settings,
+              size: 26,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            title: Text(
+              'Filters',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontSize: 24,
+                  ),
+            ),
+            onTap: () {
+              onSelectScreen('filters');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }

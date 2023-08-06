c81f86dd7aa98b8d8bc545cefeac2d535597a768
tyange <usun16@gmail.com>
2023-08-06 15:18:53 +0900

    using provider depends on other provider

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -5,7 +5,6 @@ import 'package:flutter_complete_meals_app/screens/categories.dart';
 import 'package:flutter_complete_meals_app/screens/filters.dart';
 import 'package:flutter_complete_meals_app/screens/meals.dart';
 import 'package:flutter_complete_meals_app/widgets/main_drawer.dart';
-import 'package:flutter_complete_meals_app/providers/meals_provider.dart';
 import 'package:flutter_complete_meals_app/providers/favorites_provider.dart';
 import 'package:flutter_complete_meals_app/providers/filters_provider.dart';
 
@@ -47,23 +46,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final activeFilters = ref.watch(filtersProvider);
-    final meals = ref.watch(mealsProvider);
-    final availableMeals = meals.where((meal) {
-      if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-        return false;
-      }
-      if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-        return false;
-      }
-      if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-        return false;
-      }
-      if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-        return false;
-      }
-      return true;
-    }).toList();
+    final availableMeals = ref.watch(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
